56eda199e9266ae25e4e02360e17086b35d6f2b5
asaqa988 <asaqa001@gmail.com>
2025-04-20 18:33:28 +0300

    update

---
+++ main.dart
@@ -0,0 +1,16 @@
+void main() {
+  String myname = "abedalraheem";
+
+  int myage = 32;
+
+  double myhighest = -171.4;
+
+  print(myname[0].toUpperCase());
+
+  print(myhighest.floor());
+  print(myhighest.ceil());
+  print(myhighest.round());
+  print(myhighest.isNegative);
+
+  print(myhighest.abs());
+}
